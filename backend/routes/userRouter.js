a49c8fc8f53c2688dcbc7b5c3da723580d8da3c7
Rehan <m.rd7447@gmail.com>
2022-03-06 18:56:00 +0500

    get dynamic data from database on user dashboard

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const { getAccountDetails } = require("../controller/accountController");
 const router = express.Router();
 const { registerUser, authorizeUser } = require("../controller/userController");
 // const notes = require("../data/notes");
 
 router.post("/", registerUser);
 router.post("/login", authorizeUser);
+router.get("/account", getAccountDetails);
 
 module.exports = router;
--- front-end/src/components/user/home/upperWelcome.js
+++ front-end/src/components/user/home/upperWelcome.js
@@ -1,36 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Clock from "../../clock";
 import "./upperWelcome.css";
+import Loading from "../../loading";
 
 function WelcomeMenu() {
-	const [amount] = useState("5000");
-	const [userInfo] = useState(JSON.parse(localStorage.getItem("userInfo")));
-	return (
-		<div className="mainDiv">
-			<div className="welcomeDiv">
-				<h2>Welcome, {userInfo.name}!</h2>
-				<h5>
-					<Clock />
-				</h5>
-			</div>
-			<div className="amountDiv">
-				<div className="subAmountDiv">
-					<a href="/">
-						<h1 className="amount">
-							{amount} <span>Rs</span>{" "}
-						</h1>
-					</a>
-					<br />
-					<h5 className="balanceHeading">Current Balance</h5>
-				</div>
-				<div className="subMenu">
-					<a href="/">Transfer History</a>
-					<a href="/">Recent Cheque Deposits</a>
-					<a href="/">Recent Cheque Transactions</a>
-				</div>
-			</div>
-		</div>
-	);
+  const [userInfo] = useState(JSON.parse(localStorage.getItem("userInfo")));
+  const [userAccountInfo, setUserAccountInfo] = useState({
+    balance: "Loading...",
+    IBAN: "Loading...",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const getAccountInfo = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`/users/account`, {
+        params: { accountHolder: userInfo._id },
+      });
+      setLoading(false);
+      localStorage.setItem("userAccountInfo", JSON.stringify(data));
+      setUserAccountInfo(data);
+    } catch (error) {
+      console.log("Error fecthing user account details" + error);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => getAccountInfo(), 1000);
+  }, []);
+
+  return (
+    <div className="mainDiv">
+      <div className="welcomeDiv">
+        <div>
+          <span className="iban">{userAccountInfo.IBAN}</span>
+          <h2>Welcome, {userInfo.name}!</h2>
+        </div>
+        <h5>
+          <Clock />
+        </h5>
+      </div>
+      <div className="amountDiv">
+        <div className="subAmountDiv">
+          <a href="/user">
+            {loading ? (
+              <Loading />
+            ) : (
+              <h1 className="amount">
+                {userAccountInfo.balance + " "}
+                <span>Rs</span>{" "}
+              </h1>
+            )}
+          </a>
+          <br />
+          <h5 className="balanceHeading">Current Balance</h5>
+        </div>
+        <div className="subMenu">
+          <a href="/">Transfer History</a>
+          <a href="/">Recent Cheque Deposits</a>
+          <a href="/">Recent Cheque Transactions</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WelcomeMenu;
