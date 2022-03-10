3e9adba051a57a8417e271ee227bf2611fbeaef7
Basim Ayub <basimayub.bsm@gmail.com>
2022-03-10 13:11:53 +0500

    added graph in admin

--- front-end/src/screens/admin/adminDashBoard.js
+++ front-end/src/screens/admin/adminDashBoard.js
@@ -5,21 +5,24 @@ import CustomerTable from "../../components/admin/customerTable";
 import CashierTableTable from "../../components/admin/cashierTable";
 import ErrorTable from "../../components/admin/errorTable";
 import EmployeeTable from "../../components/admin/employeeTable";
+import ApexChart from "../../components/charts/chart";
 
 export default function AdminDashboard() {
-  return (
-    <div
-      style={{
-        width: "100%",
-        display: "inline-block",
-        backgroundColor: "#F3F3F9",
-      }}
-    >
-      <AdminHeader />
-      <CustomerTable></CustomerTable>
-      <EmployeeTable></EmployeeTable>
-      <CashierTableTable></CashierTableTable>
-      <ErrorTable></ErrorTable>
-    </div>
-  );
+	return (
+		<div
+			style={{
+				width: "100%",
+				display: "inline-block",
+				backgroundColor: "#F3F3F9",
+			}}
+		>
+			<AdminHeader />
+			<ApexChart />
+
+			<CustomerTable></CustomerTable>
+			<EmployeeTable></EmployeeTable>
+			<CashierTableTable></CashierTableTable>
+			<ErrorTable></ErrorTable>
+		</div>
+	);
 }
