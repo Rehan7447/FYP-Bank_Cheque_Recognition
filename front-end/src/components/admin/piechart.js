044548c30acd67d433fb794f021128ac66718157
Rehan Ashraf <m.rd7447@gmail.com>
2022-06-15 18:06:03 +0500

    change user header

--- front-end/src/components/admin/piechart.js
+++ front-end/src/components/admin/piechart.js
@@ -2,53 +2,53 @@ import React, { Component } from "react";
 import Chart from "react-apexcharts";
 
 export default class PieChart extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			options: {
-				chart: {
-					id: "chart",
-				},
-				xaxis: {
-					categories: [
-						"JAN",
-						"FEB",
-						"MAR",
-						"APR",
-						"MAY",
-						"JUN",
-						"JUL",
-						"AUG",
-						"SEP",
-						"OCT",
-						"NOV",
-						"DEC",
-					],
-				},
-			},
-			series: [
-				{
-					name: "series-1",
-					data: [30, 50, 35, 50, 43, 60, 70, 91, 125, 130, 135, 165],
-				},
-			],
-			pieseries: [44, 55, 41, 17, 15],
-			pieoptions: {
-				chart: {
-					type: "donut",
-				},
-				labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
-			},
-		};
-	}
-	render() {
-		return (
-			<Chart
-				options={this.state.pieoptions}
-				series={this.state.pieseries}
-				type="donut"
-				height="100%"
-			/>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: {
+        chart: {
+          id: "chart",
+        },
+        xaxis: {
+          categories: [
+            "JAN",
+            "FEB",
+            "MAR",
+            "APR",
+            "MAY",
+            "JUN",
+            "JUL",
+            "AUG",
+            "SEP",
+            "OCT",
+            "NOV",
+            "DEC",
+          ],
+        },
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 50, 35, 50, 43, 60, 70, 91, 125, 130, 135, 165],
+        },
+      ],
+      pieseries: [44, 55],
+      pieoptions: {
+        chart: {
+          type: "donut",
+        },
+        labels: ["Customers", "Employees"],
+      },
+    };
+  }
+  render() {
+    return (
+      <Chart
+        options={this.state.pieoptions}
+        series={this.state.pieseries}
+        type="donut"
+        height="100%"
+      />
+    );
+  }
 }
