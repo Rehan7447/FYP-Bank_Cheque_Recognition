fce0c165e31767c8271ecf11cc098556343f0351
Rehan Ashraf <m.rd7447@gmail.com>
2022-06-15 18:39:51 +0500

    make changes to server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv");
 const userRouter = require("./routes/userRouter");
 const adminRouter = require("./routes/adminRouter");
 const cashierRouter = require("./routes/cashierRouter");
 const connectDB = require("./config/db");
+
 // const notes = require("./data/notes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleWare");
 dotenv.config();
@@ -12,19 +14,24 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get("/", (re, res) => {
-	res.send("API is running");
-});
 app.use("/admin", adminRouter);
 app.use("/users", userRouter);
 app.use("/cashier", cashierRouter);
-// app.use("/api/users", userRouter);
 
-// app.get("/api/notes", (req, res) => {
-// 	res.json(notes);
-// });
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "front-end/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"));
+  });
+} else {
+  app.get("/", (re, res) => {
+    res.send("API is running");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, console.log(`server started at port ${PORT}`));
